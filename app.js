1a0133ed795da0fcd897c293641415517fe72006
Mohamad Kazemi <kazemi.cpu@gmail.com>
2024-04-14 13:13:03 +0330

    initial project

---
+++ app.js
@@ -0,0 +1,13 @@
+let input = document.getElementById("input");
+let sum = document.getElementById("sum");
+
+
+input.addEventListener("input", function () {
+  sum.innerText = Number(input.value) + 4;
+});
+
+
+
+
+
+
